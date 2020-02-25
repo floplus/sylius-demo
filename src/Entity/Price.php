f5988d012257e03fe535e46f57736f622a86f153
Łukasz Chruściel <lchrusciel@gmail.com>
2020-02-25 14:42:02 +0100

    Default price resource

---
+++ src/Entity/Price.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Sylius\Component\Resource\Model\ResourceInterface;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="app_price")
+ */
+class Price implements ResourceInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string")
+     */
+    private $cooperative;
+
+    /**
+     * @var int|null
+     * @ORM\Column(type="integer", options={"default": 0})
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCooperative(): ?string
+    {
+        return $this->cooperative;
+    }
+
+    public function setCooperative(?string $cooperative): void
+    {
+        $this->cooperative = $cooperative;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): void
+    {
+        $this->price = $price;
+    }
+}
